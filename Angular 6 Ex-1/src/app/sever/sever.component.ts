0a4751816f7573f18b91468d44f9c04ab234a73a
Adinarayana Reddy <adhireddy.g@gmail.com>
2018-06-25 19:10:29 +0530

    Angular 6 Example 1(creating the components)

---
+++ Angular 6 Ex-1/src/app/sever/sever.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sever',
+  templateUrl: './sever.component.html',
+  styleUrls: ['./sever.component.css']
+})
+export class SeverComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
